b63254def94cbc21324736165eda9bfb3209fce3
Krushil <krushilnaik96@gmail.com>
2021-01-22 11:12:32 -0500

    shell out the ground work for the models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,43 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    pass
+	name = models.CharField(max_length=64)
+
+class AuctionListing(models.Model):
+	FASHION = "F"
+	TOYS = "T"
+	ELECTRONICS = "E"
+	HOME = "H"
+	GENERAL = "G"
+
+	CATEGORY_CHOICES = [
+		(FASHION, "Fashion"),
+		(TOYS, "Toys"),
+		(ELECTRONICS, "Electronics"),
+		(HOME, "Home"),
+		(GENERAL, "General")
+	]
+
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=256)
+	startingBid = models.FloatField()
+	imageURL = models.CharField(default="", max_length=100)
+	isActive = models.BooleanField(default=True)
+	auctioner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	category = models.CharField(
+		default=GENERAL,
+		max_length=15,
+		choices=CATEGORY_CHOICES
+	)
+
+class Bid(models.Model):
+	listing = models.ForeignKey(AuctionListing, models.CASCADE)
+	bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+	bidAmount = models.FloatField()
+
+class Comment(models.Model):
+	comment = models.CharField(max_length=120)
+	listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+	commenter = models.ForeignKey(User, on_delete=models.CASCADE)
