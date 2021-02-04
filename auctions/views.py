4332b55dc67bb6177b4635d86f6d56818e2c24fa
Krushil <krushilnaik96@gmail.com>
2021-02-04 18:41:07 -0500

    get started on the various site pages

--- auctions/views.py
+++ auctions/views.py
@@ -1,15 +1,26 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http.request import HttpRequest
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, AuctionListing
+
+class CreateListingForm(forms.Form):
+	title       = forms.CharField()
+	description = forms.CharField()
+	startingBid = forms.DecimalField(decimal_places=2)
+	imageURL    = forms.CharField(empty_value="")
+
 
 
 def index(request):
-    return render(request, "auctions/index.html")
-
+	return render(request, "auctions/index.html", {
+		"listings": AuctionListing.objects.filter(isActive=True)
+	})
 
 def login_view(request):
     if request.method == "POST":
@@ -31,6 +42,7 @@ def login_view(request):
         return render(request, "auctions/login.html")
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
@@ -61,3 +73,21 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required
+def create_listing(request: HttpRequest):
+	form = CreateListingForm(request.POST or None)
+	return render(request, "auctions/create_listing.html")
+
+@login_required
+def listing(request: HttpRequest, listing_id):
+	return render(request, "auctions/listing.html")
+
+@login_required
+def watchlist(request: HttpRequest):
+	return render(request, "auctions/watchlist.html")
+
+@login_required
+def categories(request: HttpRequest):
+	return render(request, "auctions/categories.html")
