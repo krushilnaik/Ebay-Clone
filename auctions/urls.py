d98ef23ee0d6843dc8f4fd1b574725f88cecf938
Krushil Naik <krushilnaik96@gmail.com>
2021-08-01 10:10:34 -0400

    Define some more urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index"),
-	path("login", views.login_view, name="login"),
-	path("logout", views.logout_view, name="logout"),
-	path("register", views.register, name="register"),
-	path("create_listing", views.create_listing, name="create_listing"),
-	path("watchlist", views.watchlist, name="watchlist"),
-	path("categories", views.categories, name="categories"),
-	path("listing/<int:id>", views.listing, name="listing")
+	path('', views.index, name='index'),
+	path('login', views.login_view, name='login'),
+	path('logout', views.logout_view, name='logout'),
+	path('register', views.register, name='register'),
+	path('create_listing', views.create_listing, name='create_listing'),
+	path('watchlist', views.watchlist, name='watchlist'),
+	path('categories', views.categories, name='categories'),
+	path('category/<str:category>', views.category, name='category'),
+	path('listing/<int:id>', views.listing, name='listing')
 ]
