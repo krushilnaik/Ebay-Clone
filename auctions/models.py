6052e1be59764f6d8b441843a503205427151598
Krushil Naik <krushilnaik96@gmail.com>
2021-08-01 10:12:17 -0400

    Rough draft database models

--- auctions/models.py
+++ auctions/models.py
@@ -1,46 +1,80 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 class User(AbstractUser):
 	pass
-	# displayName = models.CharField(max_length=64)
-	# username = models.CharField(max_length=20, unique=True)
-	# password = models.CharField(max_length=16)
 
-class AuctionListing(models.Model):
-	FASHION = "F"
-	TOYS = "T"
-	ELECTRONICS = "E"
-	HOME = "H"
-	GENERAL = "G"
-
+class Listing(models.Model):
 	CATEGORY_CHOICES = [
-		(FASHION, "Fashion"),
-		(TOYS, "Toys"),
-		(ELECTRONICS, "Electronics"),
-		(HOME, "Home"),
-		(GENERAL, "General")
+		('No category', 'No category'),
+		('Toys', 'Toys'),
+		('Home', 'Home'),
+		('Garden', 'Garden'),
+		('Books', 'Books')
 	]
 
-	title = models.CharField(max_length=50)
-	description = models.CharField(max_length=256)
-	startingBid = models.FloatField()
-	imageURL = models.CharField(default="", max_length=100)
-	isActive = models.BooleanField(default=True)
-	auctioner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=30000)
+	starting_bid = models.FloatField()
+	current_price = models.FloatField()
+	is_active = models.BooleanField(default=True)
+	imageURL = models.CharField(max_length=3000, blank=True) 
+	posted_date = models.DateTimeField(default=timezone.now, blank=True)
 
-	category = models.CharField(
-		default=GENERAL,
-		max_length=15,
-		choices=CATEGORY_CHOICES
+	creator = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE,
+		related_name='my_listings'
 	)
 
+	category = models.CharField(
+		max_length=50,
+		choices=CATEGORY_CHOICES,
+		default='No category'
+	) 
+
+	watched_by = models.ManyToManyField(
+		settings.AUTH_USER_MODEL,
+		blank=True,
+		related_name='watching'
+	)
+
+	def save(self, *args, **kwargs):
+		if self.current_price is None:
+			self.current_price = self.starting_bid
+
+		super(Listing, self).save(*args, **kwargs)
+
 class Bid(models.Model):
-	listing = models.ForeignKey(AuctionListing, models.CASCADE, related_name="bids")
-	bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-	bidAmount = models.FloatField()
+	bid_date = models.DateTimeField(default=timezone.now, blank=True)
+	amount = models.FloatField()
+
+	listing = models.ForeignKey(
+		Listing,
+		on_delete=models.CASCADE,
+		related_name='l_bids'
+	)
+
+	bid_user = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE, 
+		related_name = 'u_bids'
+	)
 
 class Comment(models.Model):
-	comment = models.CharField(max_length=120)
-	listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="auction_comments")
-	commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_comments")
+	comment_datetime = models.DateTimeField(default=timezone.now, blank=True)
+	text = models.CharField(max_length=30000)
+
+	listing  = models.ForeignKey(
+		Listing,
+		on_delete=models.CASCADE,
+		related_name = 'comments'
+	)
+
+	author = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE, 
+		related_name='my_comments'
+	)
